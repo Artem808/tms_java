e0bcbfe90add8f2575af3bc1963b95960748d405
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-18 21:47:55 +0300

    Update task_2.java

--- src/hw_4/task_2/task_2.java
+++ src/hw_4/task_2/task_2.java
@@ -1,7 +1,9 @@
 package hw_4.task_2;
 
+// task_2 -> Task_2
 public class task_2 {
     public static void main(String[] args) {
+        // s -> word
         String s = "alucard";
         String reverse = new StringBuffer(s).reverse().toString();
         System.out.println(reverse);
