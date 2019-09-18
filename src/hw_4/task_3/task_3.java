8b9d2274b27ff01234a9c6ec89afa6606e5a29ad
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-18 21:49:46 +0300

    Update task_3.java

--- src/hw_4/task_3/task_3.java
+++ src/hw_4/task_3/task_3.java
@@ -1,7 +1,9 @@
 package hw_4.task_3;
 
+// task_3 -> Task_3
 public class task_3 {
     public static void main(String[] args) {
+        // s -> sentence
         String s = "Green. red. blue. yellow";
         s = s.replace(".", ",");
         System.out.println(s);
