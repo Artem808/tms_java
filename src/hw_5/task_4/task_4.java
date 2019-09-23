05c60671fe6f774f44bc377c4b06711aa060caba
Artem808 <55029684+Artem808@users.noreply.github.com>
2019-09-23 19:08:43 +0300

    Update task_4.java

--- src/hw_5/task_4/task_4.java
+++ src/hw_5/task_4/task_4.java
@@ -4,9 +4,8 @@ public class task_4 {
     public static void main(String[] args) {
         int number = 6;
         for (int n = number; n > 0; n--) {
-            // for (int j = 1; j < n + 1; j++)  - зачем j=1 и при этом n+1?
-            // for (int j = 0; j < n; j++) - думаю так получше
-            for (int j = 1; j < n + 1; j++) {
+          
+        for (int j = 1; j < n + 1; j++) {
                 System.out.print(".");
             }
             System.out.println("");
