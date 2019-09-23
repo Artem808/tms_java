bf642adab75c4077810283371d7e835888d60286
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-23 21:11:04 +0300

    Update task_1.java

--- src/hw_5/task_1/task_1.java
+++ src/hw_5/task_1/task_1.java
@@ -1,7 +1,7 @@
 package hw_5.task_1;
 
 import java.util.Arrays;
-
+// task_1 -> Task_1
 public class task_1 {
     public static void main(String[] args) {
         int[] arr = new int[101];
@@ -17,10 +17,11 @@ public class task_1 {
             if (i % 2 == 0) {
                 even = even + i;
             }
-            
+            // пустая строка не нужна
             if (i % 2 != 0) {
                 odd = odd + i;
             }
+            // зачем каждый раз в цикле считать сумму? это можно сделать 1 раз за его пределами
             sum = even + odd;
         }
 
