87a99720d7a8efe80fc2db5ef722f2505da115dc
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-23 21:27:37 +0300

    Update task_4.java

--- src/hw_5/task_4/task_4.java
+++ src/hw_5/task_4/task_4.java
@@ -1,11 +1,13 @@
 package hw_5.task_4;
 
+// task_4 -> Task_4
 public class task_4 {
     public static void main(String[] args) {
         int number = 6;
         for (int n = number; n > 0; n--) {
-          
+          // путая строка не нужна
         for (int j = 1; j < n + 1; j++) {
+            // for (int j = 0; j < n; j++) {
                 System.out.print(".");
             }
             System.out.println("");
